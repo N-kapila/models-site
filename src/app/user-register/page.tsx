f3038953577679904493c89f1ff99c6e58d2531d
N-kapila <nirmalkapilarathne1998@gmail.com>
2024-08-07 13:48:05 +0530

    register page completed

--- src/app/user-register/page.tsx
+++ src/app/user-register/page.tsx
@@ -10,9 +10,19 @@ import {
   Divider,
   Typography,
   TextField,
+  Link,
+  MenuItem,
+  FormControl,
+  Select,
+  SelectChangeEvent,
+  InputAdornment,
+  IconButton,
 } from "@mui/material";
+import Image from "next/image";
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import GoogleIcon from "@mui/icons-material/Google";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+//import registerimg from "../../../public/assets/registerimg.jpg";
+import googleIcon from "../../../public/assets/LogoGoogle.png";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -20,36 +30,78 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.secondary,
-  height: "100vh",
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-between",
   alignItems: "center",
 }));
 
-const DottedDivider = styled(Divider)({
+const DottedDivider = styled(Divider)(({ theme }) => ({
   width: "100%",
   borderBottom: "1px dotted rgba(0, 0, 0, 0.12)",
   margin: "16px 0",
-});
+  [theme.breakpoints.up("lg")]: {
+    display: "none",
+  },
+}));
 
 const textFieldStyle = {
   marginBottom: "16px",
   "& .MuiInputBase-root": {
     height: "40px",
+    display: "flex",
+    alignItems: "center",
+  },
+  "& .MuiInputLabel-root": {
+    top: "-6px",
   },
 };
 
 export default function Page() {
+  const [role, setRole] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setRole(event.target.value);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item sm={6} sx={{ display: { xs: "none", sm: "block" } }}>
-          <Item>xs=6 md=4</Item>
+          <Box sx={{ position: "relative", width: "100%", height: "100%" }}>
+            {/* <Image
+              src={registerimg}
+              alt="register image"
+              layout="fill"
+              objectFit="cover"
+            /> */}
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Item>
-            <Typography variant="h4" gutterBottom>
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                mb: 5,
+              }}
+            >
+              <Typography variant="subtitle1" gutterBottom sx={{ mr: 1 }}>
+                Already have an account?
+              </Typography>
+              <Link href="#" variant="subtitle1" underline="none">
+                Login
+              </Link>
+            </Box>
+
+            <Typography variant="h4" gutterBottom sx={{ color: "#000000" }}>
               Create Your Account
             </Typography>
             <Box sx={{ width: "90%" }}>
@@ -62,7 +114,7 @@ export default function Page() {
               </Typography>
               <TextField
                 fullWidth
-                label="Enter Your Name"
+                // label="Enter Your Name"
                 id="name"
                 sx={textFieldStyle}
               />
@@ -76,7 +128,7 @@ export default function Page() {
               </Typography>
               <TextField
                 fullWidth
-                label="Enter Your Email"
+                //label="Enter Your Email"
                 id="email"
                 sx={textFieldStyle}
               />
@@ -90,7 +142,7 @@ export default function Page() {
               </Typography>
               <TextField
                 fullWidth
-                label="Enter Your Phone Number"
+                // label="Enter Your Phone Number"
                 id="phoneNumber"
                 sx={textFieldStyle}
               />
@@ -102,12 +154,23 @@ export default function Page() {
               >
                 Role
               </Typography>
-              <TextField
+              <FormControl
                 fullWidth
-                label="Enter Your Email"
-                id="email"
-                sx={textFieldStyle}
-              />
+                size="small"
+                sx={{ mb: 2, textAlign: "left" }}
+              >
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={role}
+                  // label="Age"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>Photographer</MenuItem>
+                  <MenuItem value={20}>Model</MenuItem>
+                  <MenuItem value={20}>Designer</MenuItem>
+                </Select>
+              </FormControl>
 
               <Typography
                 variant="subtitle2"
@@ -118,26 +181,107 @@ export default function Page() {
               </Typography>
               <TextField
                 fullWidth
-                label="Enter Your Password"
                 id="password"
+                type={showPassword ? "text" : "password"}
                 sx={textFieldStyle}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        edge="end"
+                        onClick={handleClickShowPassword}
+                        aria-label={
+                          showPassword ? "Hide password" : "Show password"
+                        }
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               />
+
+              <Typography
+                variant="subtitle2"
+                gutterBottom
+                sx={{ textAlign: "left" }}
+              >
+                Confirm Password
+              </Typography>
+              <TextField
+                fullWidth
+                id="confirm-password"
+                type={showPassword ? "text" : "password"}
+                sx={textFieldStyle}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        edge="end"
+                        onClick={handleClickShowPassword}
+                        aria-label={
+                          showPassword ? "Hide password" : "Show password"
+                        }
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+
+              <Button variant="contained" sx={{ margin: 3 }}>
+                Register
+              </Button>
             </Box>
+
             <DottedDivider />
-            <Button
-              component="label"
-              variant="outlined"
-              startIcon={<GoogleIcon />}
+
+            <Box sx={{ display: { xs: "none", lg: "block" } }}>
+              <p>
+                -------------------------------------------------------- OR
+                --------------------------------------------------------
+              </p>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "35%",
+                m: 3,
+              }}
             >
-              <b>Google</b>
-            </Button>
-            <Button
-              component="label"
-              variant="outlined"
-              startIcon={<FacebookRoundedIcon />}
-            >
-              <b>Facebook</b>
-            </Button>
+              <Button
+                size="small"
+                component="label"
+                variant="outlined"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  // paddingLeft: 8,
+                  // paddingRight: 8,
+                }}
+              >
+                <Image
+                  src={googleIcon}
+                  alt="Google Icon"
+                  width={15}
+                  height={15}
+                  style={{ marginRight: 8 }}
+                />
+                <b>Google</b>
+              </Button>
+              <br />
+              <Button
+                size="small"
+                component="label"
+                variant="outlined"
+                startIcon={<FacebookRoundedIcon />}
+              >
+                <b> Facebook</b>
+              </Button>
+            </Box>
           </Item>
         </Grid>
       </Grid>
